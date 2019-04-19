f5c6b72aa1c03342a3f91c665f8e2218d55d948d
Mohamed Ali <mali30@student.gsu.edu>
2019-04-19 19:03:37 -0400

    Final Version of Project. Add OS features next

---
+++ app/src/main/java/com/code/mohamedali/osproj/NoteEditorActivity.java
@@ -0,0 +1,76 @@
+package com.code.mohamedali.osproj;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    int noteID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+
+        // Displays document that the user has selected
+        EditText editText = (EditText)findViewById(R.id.editText);
+
+        // Gets NoteID that passed to this activity from MainActivity
+
+        Intent intent = getIntent();
+         noteID = intent.getIntExtra("noteID", -1);
+
+        if(noteID != -1){
+            editText.setText(MainActivity.notes.get(noteID));
+        }else{
+             // Add a new note
+
+            // creating empty note
+            MainActivity.notes.add("");
+            noteID = MainActivity.notes.size() -1;
+            // updating array adapter for list view
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        }
+        // Listens to when you change your text
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                // Updates Notes ArrayList from the Main Activity and then automatically saves them
+                MainActivity.notes.set(noteID,String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+
+
+                // Saving Notes
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.code.mohamedali.osproj"
+                , Context.MODE_PRIVATE);
+
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes" , set).apply();
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
